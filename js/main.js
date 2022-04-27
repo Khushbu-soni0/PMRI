8cb27bd628fbd49ab61038213ad91a2398b5ce59
Khushbu Soni <61386783+Khushbu-soni0@users.noreply.github.com>
2022-04-27 09:35:57 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,37 @@ const myCarousel = document.getElementById('myCarousel')
     document.querySelector(".testi-img img").src = activeItem.getAttribute("data-img");
     document.querySelector(".testi-img .circle").style.backgroundColor = activeItem.getAttribute("data-color");
   })
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+
+// Requiring fs module in which
+// readFile function is defined.
+const fs = require('fs')
+  
+// Reading data in utf-8 format
+// which is a type of character set.
+// Instead of 'utf-8' it can be 
+// other character set also like 'ascii'
+fs.readFile('Input.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+  
+    // Converting Raw Buffer to text
+    // data using tostring function.
+    console.log(data);
+})
