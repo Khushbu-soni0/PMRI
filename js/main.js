90370632e6f3e43932382cb66c028b295adbede7
Khushbu Soni <61386783+Khushbu-soni0@users.noreply.github.com>
2022-05-10 12:18:25 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -38,3 +38,23 @@ fs.readFile('Input.txt', 'utf-8', (err, data) => {
     // data using tostring function.
     console.log(data);
 })
+
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.href.includes(current)) {
+    li.classList.add('active');
+    }
+  });
+};
